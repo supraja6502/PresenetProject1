19566a025f5fa617f5dec4cf5c1baf0da7ccc9b0
supraja6502 <s.supraja6502@gmail.com>
2022-11-16 11:17:19 +0530

    Demo for Team

---
+++ src/main/java/com/kmit/Webproject/Applivcation.java
@@ -0,0 +1,11 @@
+package com.kmit.Webproject;
+
+public class Applivcation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
